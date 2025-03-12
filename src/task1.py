acb54502b590f45f16d1b75f2ecf37166154f810
EASONTAN03 <129657923+EASONTAN03@users.noreply.github.com>
2025-03-12 10:53:12 +0800

    push task4

--- src/task1.py
+++ src/task1.py
@@ -1,161 +1,161 @@
-# import warnings
-# warnings.simplefilter(action='ignore', category=UserWarning)
-# warnings.simplefilter(action='ignore', category=FutureWarning)
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# import numpy as np
-# import pandas as pd
-# from pmdarima import auto_arima
-# from tqdm import tqdm  # Progress bar
+import numpy as np
+import pandas as pd
+from pmdarima import auto_arima
+from tqdm import tqdm  # Progress bar
 
-# data_path = r"data/world_bank_data_dev.csv"
-# df = pd.read_csv(data_path)
-# df["date"] = pd.to_datetime(df["date"]).dt.year
+data_path = r"data/world_bank_data_dev.csv"
+df = pd.read_csv(data_path)
+df["date"] = pd.to_datetime(df["date"]).dt.year
 
 
-# from sklearn.impute import KNNImputer
-# def impute_knn(df):
-#     """Impute missing values using KNN with data from all countries."""
-#     imputer = KNNImputer(n_neighbors=3)
-#     numeric_df = df.select_dtypes(include=[np.number])
-#     imputed_data = imputer.fit_transform(numeric_df)
-#     imputed_df = pd.DataFrame(imputed_data, columns=numeric_df.columns, index=numeric_df.index)
-#     for col in df.columns:
-#         if col not in imputed_df.columns:
-#             imputed_df[col] = df[col]
-#     return imputed_df
+from sklearn.impute import KNNImputer
+def impute_knn(df):
+    """Impute missing values using KNN with data from all countries."""
+    imputer = KNNImputer(n_neighbors=3)
+    numeric_df = df.select_dtypes(include=[np.number])
+    imputed_data = imputer.fit_transform(numeric_df)
+    imputed_df = pd.DataFrame(imputed_data, columns=numeric_df.columns, index=numeric_df.index)
+    for col in df.columns:
+        if col not in imputed_df.columns:
+            imputed_df[col] = df[col]
+    return imputed_df
 
-# def identify_missing_sequences(matrix):
-#     """ Identify missing sequences in each column of a matrix """
-#     is_nan = np.isnan(matrix)  # Boolean mask for missing values
-#     missing_indices = np.zeros_like(matrix, dtype=int)  # Store missing sequence indices
-#     # Compute missing groups (vectorized)
-#     for col in range(matrix.shape[1]):
-#         series=matrix[:, col]
-#         series = np.asarray(series)  # Convert to NumPy array
-#         is_nan = np.isnan(series)  # Identify missing values (NaN)
+def identify_missing_sequences(matrix):
+    """ Identify missing sequences in each column of a matrix """
+    is_nan = np.isnan(matrix)  # Boolean mask for missing values
+    missing_indices = np.zeros_like(matrix, dtype=int)  # Store missing sequence indices
+    # Compute missing groups (vectorized)
+    for col in range(matrix.shape[1]):
+        series=matrix[:, col]
+        series = np.asarray(series)  # Convert to NumPy array
+        is_nan = np.isnan(series)  # Identify missing values (NaN)
         
-#         # Fix: Ensure the first NaN is also counted
-#         missing_groups = np.cumsum(is_nan & np.concatenate(([True], ~is_nan[:-1])))  # Fix for first NaN
-#         missing_indices[:, col] = is_nan * missing_groups  # Assign sequence numbers
+        # Fix: Ensure the first NaN is also counted
+        missing_groups = np.cumsum(is_nan & np.concatenate(([True], ~is_nan[:-1])))  # Fix for first NaN
+        missing_indices[:, col] = is_nan * missing_groups  # Assign sequence numbers
 
-#     return missing_indices
+    return missing_indices
 
-# def auto_arima_impute(series, inverse=False):
-#     """ Auto ARIMA-based imputation for missing sequences """
-#     if not inverse:
-#         series = pd.Series(series).sort_values(ascending=True)
-#         valid_data = series.dropna()
+def auto_arima_impute(series, inverse=False):
+    """ Auto ARIMA-based imputation for missing sequences """
+    if not inverse:
+        series = pd.Series(series).sort_values(ascending=True)
+        valid_data = series.dropna()
 
-#         # Fit ARIMA on valid values
-#         model = auto_arima(valid_data, seasonal=False, suppress_warnings=True)
+        # Fit ARIMA on valid values
+        model = auto_arima(valid_data, seasonal=False, suppress_warnings=True)
 
-#         # Predict missing values
-#         missing_idx = np.where(series.isna())[0]
+        # Predict missing values
+        missing_idx = np.where(series.isna())[0]
 
-#         # print(series)
-#         # print(np.where(series.isna()))
-#         # print(missing_idx)
-#         predictions = model.predict(n_periods=len(missing_idx))
-#         predictions = np.flip(predictions)
+        # print(series)
+        # print(np.where(series.isna()))
+        # print(missing_idx)
+        predictions = model.predict(n_periods=len(missing_idx))
+        predictions = np.flip(predictions)
 
-#     if inverse:
-#         series = pd.Series(series)
-#         valid_data = series.dropna()
+    if inverse:
+        series = pd.Series(series)
+        valid_data = series.dropna()
 
-#         # Fit ARIMA on valid values
-#         valid_data = np.log1p(valid_data)
-#         model = auto_arima(valid_data, seasonal=False, suppress_warnings=True)
+        # Fit ARIMA on valid values
+        valid_data = np.log1p(valid_data)
+        model = auto_arima(valid_data, seasonal=False, suppress_warnings=True)
 
-#         # Predict missing values
-#         missing_idx = np.where(series.isna())[0]
+        # Predict missing values
+        missing_idx = np.where(series.isna())[0]
 
-#         # print(series)
-#         # print(np.where(series.isna()))
-#         # print(missing_idx)
-#         predictions = model.predict(n_periods=len(missing_idx))
-#         predictions = np.expm1(model.predict(n_periods=len(missing_idx)))
+        # print(series)
+        # print(np.where(series.isna()))
+        # print(missing_idx)
+        predictions = model.predict(n_periods=len(missing_idx))
+        predictions = np.expm1(model.predict(n_periods=len(missing_idx)))
 
-#     return np.array(predictions)
+    return np.array(predictions)
 
-# def impute_missing_values(matrix):
-#     """ Main function for matrix-based imputation """
-#     matrix = np.array(matrix, dtype=float)  # Convert to NumPy array
-#     missing_indices = identify_missing_sequences(matrix)
+def impute_missing_values(matrix):
+    """ Main function for matrix-based imputation """
+    matrix = np.array(matrix, dtype=float)  # Convert to NumPy array
+    missing_indices = identify_missing_sequences(matrix)
     
-#     for col in range(matrix.shape[1]):
-#         col_data = matrix[:, col]
-#         missing_seq = np.unique(missing_indices[:, col])
-#         for seq in missing_seq:
-#             is_nan=np.isnan(col_data)
-#             if seq == 0:
-#                 continue  # Skip if not missing
+    for col in range(matrix.shape[1]):
+        col_data = matrix[:, col]
+        missing_seq = np.unique(missing_indices[:, col])
+        for seq in missing_seq:
+            is_nan=np.isnan(col_data)
+            if seq == 0:
+                continue  # Skip if not missing
             
-#             indices = np.where(missing_indices[:, col] == seq)[0]
-#             start, end, length = indices[0], indices[-1], len(indices)
-#             if length == 1:
-#                 if start == 0:
-#                     col_data[start] = col_data[start+1]
-#                 elif end == 43:
-#                     col_data[end] = col_data[end-1]
-#                 else:
-#                     col_data[start] = (col_data[start+1]+col_data[start-1])/2
-#             elif length == 2 and (start-1) >=0 and (end+1) <44:
-#                 col_data[start] = col_data[start-1]
-#                 col_data[end] = col_data[end+1]
-#             elif length<15 and start == 0 and np.sum(is_nan[end+1:end+21])==0:
-#                 arima_pred = auto_arima_impute(col_data[start:end+21].flatten())
-#                 col_data[start:end+1] = arima_pred
-#             elif length<15 and end == 43 and np.sum(is_nan[start-15:start])==0:
-#                 arima_pred = auto_arima_impute(col_data[start-15:end+1].flatten(), inverse=True)
-#                 col_data[start:end+1] = arima_pred
-#             elif length != 44:
-#                 try:
-#                     if (end+1) <=43:
-#                             col_data[start:end+1] = col_data[end + 1]
-#                     elif (start-1) >=0:
-#                         col_data[start:end+1] = col_data[start - 1]
-#                 except Exception:
-#                     try:
-#                         col_data[start:end+1] = np.mean(col_data[~np.isnan(col_data)]) 
-#                     except Exception:
-#                         continue
+            indices = np.where(missing_indices[:, col] == seq)[0]
+            start, end, length = indices[0], indices[-1], len(indices)
+            if length == 1:
+                if start == 0:
+                    col_data[start] = col_data[start+1]
+                elif end == 43:
+                    col_data[end] = col_data[end-1]
+                else:
+                    col_data[start] = (col_data[start+1]+col_data[start-1])/2
+            elif length == 2 and (start-1) >=0 and (end+1) <44:
+                col_data[start] = col_data[start-1]
+                col_data[end] = col_data[end+1]
+            elif length<15 and start == 0 and np.sum(is_nan[end+1:end+21])==0:
+                arima_pred = auto_arima_impute(col_data[start:end+21].flatten())
+                col_data[start:end+1] = arima_pred
+            elif length<15 and end == 43 and np.sum(is_nan[start-15:start])==0:
+                arima_pred = auto_arima_impute(col_data[start-15:end+1].flatten(), inverse=True)
+                col_data[start:end+1] = arima_pred
+            elif length != 44:
+                try:
+                    if (end+1) <=43:
+                            col_data[start:end+1] = col_data[end + 1]
+                    elif (start-1) >=0:
+                        col_data[start:end+1] = col_data[start - 1]
+                except Exception:
+                    try:
+                        col_data[start:end+1] = np.mean(col_data[~np.isnan(col_data)]) 
+                    except Exception:
+                        continue
             
-#         matrix[:, col] = col_data
+        matrix[:, col] = col_data
 
-#     return matrix
+    return matrix
 
 
-# processed_df_list = []
-# for country in tqdm(df['country'].unique(), desc="Processing Countries", unit="country"):
-#     # print(f"Processing {country}...")
-#     country_df = df[df['country'] == country].copy()
-#     country_labels = country_df[["country"]]
-#     numerical_columns = country_df.drop(columns=["country","date"])
+processed_df_list = []
+for country in tqdm(df['country'].unique(), desc="Processing Countries", unit="country"):
+    # print(f"Processing {country}...")
+    country_df = df[df['country'] == country].copy()
+    country_labels = country_df[["country"]]
+    numerical_columns = country_df.drop(columns=["country","date"])
 
-#     imputed_matrix = impute_missing_values(numerical_columns)
-#     imputed_df = pd.DataFrame(imputed_matrix, columns=numerical_columns.columns)
-#     imputed_df = pd.concat([country_labels.reset_index(drop=True), imputed_df], axis=1)
+    imputed_matrix = impute_missing_values(numerical_columns)
+    imputed_df = pd.DataFrame(imputed_matrix, columns=numerical_columns.columns)
+    imputed_df = pd.concat([country_labels.reset_index(drop=True), imputed_df], axis=1)
 
-#     processed_df_list.append(imputed_df)
+    processed_df_list.append(imputed_df)
 
-# output_imputed_df = pd.concat(processed_df_list, ignore_index=True)
+output_imputed_df = pd.concat(processed_df_list, ignore_index=True)
 
-# # Retrieve original date values for each row using country as key
-# date_mapping = np.array(df['date'])
+# Retrieve original date values for each row using country as key
+date_mapping = np.array(df['date'])
 
-# # Convert date to integer (if it's not already)
-# output_imputed_df['date'] = date_mapping
+# Convert date to integer (if it's not already)
+output_imputed_df['date'] = date_mapping
 
-# # Rearrange columns: Move 'date' to the second position
-# cols = output_imputed_df.columns.tolist()
-# cols.remove('date')  # Remove 'date' from its current position
-# cols.insert(1, 'date')  # Insert 'date' as the second column
-# output_imputed_df = output_imputed_df[cols]  # Reorder dataframe
+# Rearrange columns: Move 'date' to the second position
+cols = output_imputed_df.columns.tolist()
+cols.remove('date')  # Remove 'date' from its current position
+cols.insert(1, 'date')  # Insert 'date' as the second column
+output_imputed_df = output_imputed_df[cols]  # Reorder dataframe
 
-# final_imputed_df = impute_knn(output_imputed_df)
-# final_imputed_df = final_imputed_df.reindex(columns=['country'] + [col for col in final_imputed_df.columns if col != 'country'])
+final_imputed_df = impute_knn(output_imputed_df)
+final_imputed_df = final_imputed_df.reindex(columns=['country'] + [col for col in final_imputed_df.columns if col != 'country'])
 
-# final_imputed_df.to_csv(r"data/final_impute_world_bank_data_dev.csv",index=False)
+final_imputed_df.to_csv(r"data/final_impute_world_bank_data_dev.csv",index=False)
 
 import pandas as pd
 import numpy as np
